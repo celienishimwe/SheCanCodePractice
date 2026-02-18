e4c6c9164ac3b1d9b7865ea908bc78b5e6d357cf
nishimwecelie <nishimwecelie@gmail.com>
2026-02-18 17:49:20 +0200

    Static,Instance and Constructor references done

--- StaticsInstanceConsctuctorReference/Main.java
+++ StaticsInstanceConsctuctorReference/Main.java
@@ -1,12 +1,26 @@
 package FullPractice.StaticsInstanceConsctuctorReference;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> arr1 = List.of("nishimwe","Celie");
 
-        arr1.stream().map(StringUtils::reverseString).forEach(System.out::println);
+        List<String> names = List.of("celie", "nishimwe", "cyiza");
+        List<String> arr1 = List.of("nishimwe", "Celie");
+
+        System.out.println("Printing names directly from the list:");
+        names.forEach(name -> System.out.println(name));
+
+
+        List<Person> people = names.stream()
+                .map(Person::new)
+                .collect(Collectors.toList());
+
+        people.forEach(Person::sayHello);
+        
+        arr1.stream()
+                .map(StringUtils::reverseString)
+                .forEach(System.out::println);
     }
 }
--- StaticsInstanceConsctuctorReference/StringUtils.java
+++ StaticsInstanceConsctuctorReference/StringUtils.java
@@ -4,14 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class StringUtils {
-    public static List<String> reverseString(String [] a){
-
-         List<String> reverse = new ArrayList<>();
-        for (int i=a.length-1; i>=0; i--){
-            reverse.add(a[i]);
-
-        }
-
-        return  reverse;
+    public static String reverseString(String a){
+        return new StringBuilder(a).reverse().toString();
     }
 }
